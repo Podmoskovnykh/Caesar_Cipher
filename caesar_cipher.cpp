cced7921521687015ce50569cae99ccbe6f091f0
Podmoskovnykh <100851319+Podmoskovnykh@users.noreply.github.com>
2024-03-11 21:05:41 +0300

    Update caesar_cipher.cpp

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -23,7 +23,7 @@ int main() {
     cout << "Enter your message: ";
     getline(cin, message);
 
-    int shift = 4;
+    int shift = 3;
 
     string encryptedMessage = encryptCaesarCipher(message, shift);
 
